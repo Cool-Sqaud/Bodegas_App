863a88518eb4c2f69737ccc56f16b3f84a08a785
StandeMan <s.freese@st.hanze.nl>
2023-05-30 12:31:43 +0200

    Navbar uitgebreid !!!Nog niet klaar!!!

--- src/app/public/public.component.ts
+++ src/app/public/public.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './../_services/auth.service';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-public',
   templateUrl: './public.component.html',
   styleUrls: ['./public.component.css']
 })
-export class PublicComponent {
+export class PublicComponent implements OnInit {
+  isLoggedIn: boolean = this.authService.isLoggedIn;
+  role: Number = 0;
 
+  constructor(
+    private router: Router, 
+    private authService: AuthService
+  ) { }
+
+  ngOnInit(): void {
+    this.authService.getPermissionLevel().subscribe(
+      result => this.role = result
+    )
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.loginRefresh();
+      }
+    })
+  }
+
+  private loginRefresh = (): boolean => this.isLoggedIn = this.authService.refreshLoggedIn();
 }
