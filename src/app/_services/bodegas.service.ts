9345882498e7f4762421f2488e83502eb87ace5a
luckyluuk28 <luukvdstruik.lvds@gmail.com>
2023-06-06 20:57:32 +0200

    Added Bodegas Service

---
+++ src/app/_services/bodegas.service.ts
@@ -0,0 +1,25 @@
+import { environment } from './../../environments/environment';
+import { map, catchError, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BodegasService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  // Example function (subscribe to it to be able to use the data);
+  public getMeasurements() {
+    return this.http.get(`${environment.API_URL}/measurements`).pipe(
+      map(result => result),
+      catchError(() => {
+        return of(false);
+      })
+    );
+  }
+}
+
